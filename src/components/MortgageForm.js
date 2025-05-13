ed1810d7f63da6989f546681dedf2e15ebd4b2b3
Lorenzo91347 <lorenzo.losito91@gmail.com>
2025-05-13 16:40:43 +0100

    feat:form styling part 2

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -7,11 +7,28 @@ const MortgageForm = () => {
         <h2>Mortgage Calculator</h2>
         <button>Clear All</button>
       </div>
+
       <div className='mortgage-amount'>
         <label for='amount' className='label'>Mortage Amount</label>
         <div className='amount'>
           <i className='icon'>£</i>
           <input className='input-field' type="text" name="amount"></input>
+        </div>
+      </div>
+      <div className='mort-term-rate'>
+        <div className='field-box'>
+           <label for='term' className='label'>Mortage Term</label>
+          <div className='term-rate field'>
+            <input className='input-field' type="text" name="amount"></input>
+            <i className='icon'>£</i>
+          </div>
+        </div>
+        <div className='field-box'>
+           <label for='rate' className='label'>Interest Rate</label>
+          <div className='term-rate-field'>
+            <input className='input-field' type="text" name="amount"></input>
+            <i className='icon'>£</i>
+          </div>
         </div>
       </div>
     </div>

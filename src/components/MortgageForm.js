5fd010586efd511fe52888eaa57c1dac15de5cdc
Lorenzo91347 <lorenzo.losito91@gmail.com>
2025-05-16 14:38:06 +0100

    feat:radios styling

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+let amount = 0;
+let repayment = 0;
+let interest = 0;
+
+
 const MortgageForm = () => {
   return (
     <div className='mort-form'>
@@ -19,16 +24,30 @@ const MortgageForm = () => {
         <div className='field-box'>
             <label for='term' className='label'>Mortage Term</label>
             <div className='term-rate-field'>
-            <input className='input-field-2' type="text" name="amount"></input>
+            <input className='input-field-2' type="text" name="term"></input>
             <i className='icon'>Years</i>
           </div>
         </div>
         <div className='field-box'>
             <label for='rate' className='label'>Interest Rate</label>
             <div className='term-rate-field'>
-            <input className='input-field-2' type="text" name="amount"></input>
+            <input className='input-field-2' type="text" name="rate"></input>
             <i className='icon'>%</i>
           </div>
+        </div>
+      </div>
+      <div className='mort-type'>
+        <label for='type' className='label'>Mortgage Type</label>
+        <div className='radios-cnt'>
+          <div className='radio-single'>
+            <input type='radio' value={repayment}></input>
+            <label for='repayment' className='radio-label'>Repayment</label>
+          </div>
+          <div className='radio-single'>
+            <input type='radio' value={interest}></input>
+            <label for='interest' className='radio-label'>Interest Only</label>
+          </div>
+          
         </div>
       </div>
     </div>

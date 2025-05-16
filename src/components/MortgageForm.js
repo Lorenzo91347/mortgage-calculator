9e6a3c768542c386bea7de8bd055b3fe0306da00
Lorenzo91347 <lorenzo.losito91@gmail.com>
2025-05-16 13:57:45 +0100

    feat:css chore 2

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -12,22 +12,22 @@ const MortgageForm = () => {
         <label for='amount' className='label'>Mortage Amount</label>
         <div className='amount'>
           <i className='icon-top'>£</i>
-          <input className='input-field' type="text" name="amount"></input>
+          <input className='input-field-1' type="text" name="amount"></input>
         </div>
       </div>
       <div className='mort-term-rate'>
         <div className='field-box'>
             <label for='term' className='label'>Mortage Term</label>
             <div className='term-rate-field'>
-            <input className='input-field' type="text" name="amount"></input>
+            <input className='input-field-2' type="text" name="amount"></input>
             <i className='icon'>Years</i>
           </div>
         </div>
         <div className='field-box'>
             <label for='rate' className='label'>Interest Rate</label>
             <div className='term-rate-field'>
-            <input className='input-field' type="text" name="amount"></input>
-            <i className='icon'>£</i>
+            <input className='input-field-2' type="text" name="amount"></input>
+            <i className='icon'>%</i>
           </div>
         </div>
       </div>
